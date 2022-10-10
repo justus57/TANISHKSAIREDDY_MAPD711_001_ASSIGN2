6afbad14e044daf8a4d59972a96a832e28f77e6e
Joseph K Justus <justuskasyoki57@gmail.com>
2022-10-10 14:52:00 +0300

    Initial commit

---
+++ app/src/main/java/com/justus/tanishksaireddy_mapd711_001_assign2/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.justus.tanishksaireddy_mapd711_001_assign2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class MainActivity : AppCompatActivity() {
+    lateinit var Samsung: Button
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+    }
+}
